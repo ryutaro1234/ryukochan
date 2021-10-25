fece8609de56be01ec71b7aeee3f9c37ca329b69
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-10-25 21:24:17 +0900

    Update english.js

--- college-transfer/english/english.js
+++ college-transfer/english/english.js
@@ -9,3 +9,14 @@ $(function () {
         $('span').show();
     });
 });
+
+$(function(){
+  $('a[href^="#"]').click(function(){
+    let speed = 500;
+    let href= $(this).attr("href");
+    let target = $(href == "#" || href == "" ? 'html' : href);
+    let position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+});
