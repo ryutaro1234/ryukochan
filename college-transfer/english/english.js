2127ba9e5b44e98fd031c59d8fdc2d63bb8a41f0
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-10-25 21:19:48 +0900

    a

---
+++ college-transfer/english/english.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.btn-hide').on('click', () => {
+        $('span').hide();
+    });
+});
+
+$(function () {
+    $('.btn-show').on('click', () => {
+        $('span').show();
+    });
+});
