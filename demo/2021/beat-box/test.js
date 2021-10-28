6fbd3fa6b77413523ec3b35e84b2a8277c5d6601
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-10-28 12:55:16 +0900

    update

---
+++ demo/2021/beat-box/test.js
@@ -0,0 +1,72 @@
+$(function() {
+  $("#box1_1").click(function(){
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+    }
+    else if (!$(this).hasClass("active")) {
+      $(this).addClass("active");
+    }
+  });
+});
+
+$(function() {
+  $("#box1_2").click(function(){
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+    }
+    else if (!$(this).hasClass("active")) {
+      $(this).addClass("active");
+    }
+  });
+});
+
+$(function() {
+  $("#box1_3").click(function(){
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+    }
+    else if (!$(this).hasClass("active")) {
+      $(this).addClass("active");
+    }
+  });
+});
+
+
+$(function() {
+  $("#beat_box02 a").click(function(){
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+    }
+    else if (!$(this).hasClass("active")) {
+      $(this).addClass("active");
+    }
+  });
+});
+
+
+$(function() {
+  $("#beat_box03 a").click(function(){
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+    }
+    else if (!$(this).hasClass("active")) {
+      $(this).addClass("active");
+    }
+  });
+});
+
+$(function() {
+  $("#play01").click(function(){
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this).text("▶︎再生");
+      $('#audiobox1').get(0).pause();
+      $('#audiobox1').get(0).currentTime = 0;
+    }
+    else if (!$(this).hasClass("active")) {
+      $(this).addClass("active");
+      $(this).text("■再生");
+      $('#audiobox1').get(0).play();
+    }
+  });
+});
