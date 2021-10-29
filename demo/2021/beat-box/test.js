99544a9cc5beb67932557c594a1180df2abbf10d
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2021-10-29 11:01:21 +0900

    update

--- demo/2021/beat-box/test.js
+++ demo/2021/beat-box/test.js
@@ -38,41 +38,108 @@ $(function() {
 });
 
 
+
 $(function() {
-  $("#beat_box02 a").click(function(){
+  $("#box2_1").click(function(){
     if ($(this).hasClass("active")) {
       $(this).removeClass("active");
     }
     else if (!$(this).hasClass("active")) {
       $(this).addClass("active");
+      $("#box2_2").removeClass("active");
+      $("#box2_3").removeClass("active");
     }
   });
 });
 
-
 $(function() {
-  $("#beat_box03 a").click(function(){
+  $("#box2_2").click(function(){
     if ($(this).hasClass("active")) {
       $(this).removeClass("active");
     }
     else if (!$(this).hasClass("active")) {
       $(this).addClass("active");
+      $("#box2_1").removeClass("active");
+      $("#box2_3").removeClass("active");
     }
   });
 });
+
+$(function() {
+  $("#box2_3").click(function(){
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+    }
+    else if (!$(this).hasClass("active")) {
+      $(this).addClass("active");
+      $("#box2_1").removeClass("active");
+      $("#box2_2").removeClass("active");
+    }
+  });
+});
+
+
+
+$(function() {
+  $("#box3_1").click(function(){
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+    }
+    else if (!$(this).hasClass("active")) {
+      $(this).addClass("active");
+      $("#box3_2").removeClass("active");
+      $("#box3_3").removeClass("active");
+    }
+  });
+});
+
+$(function() {
+  $("#box3_2").click(function(){
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+    }
+    else if (!$(this).hasClass("active")) {
+      $(this).addClass("active");
+      $("#box3_1").removeClass("active");
+      $("#box3_3").removeClass("active");
+    }
+  });
+});
+
+$(function() {
+  $("#box3_3").click(function(){
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+    }
+    else if (!$(this).hasClass("active")) {
+      $(this).addClass("active");
+      $("#box3_1").removeClass("active");
+      $("#box3_2").removeClass("active");
+    }
+  });
+});
+
+
 
 $(function() {
   $("#play01").click(function(){
     if ($(this).hasClass("active")) {
       $(this).removeClass("active");
       $(this).text("▶︎再生");
-      $('#audiobox1').get(0).pause();
-      $('#audiobox1').get(0).currentTime = 0;
-    }
+    }//
     else if (!$(this).hasClass("active")) {
       $(this).addClass("active");
       $(this).text("■再生");
-      $('#audiobox1').get(0).play();
-    }
+
+      if ($("#box1_1").hasClass("active")) {
+          new Audio("c.mp3").play()
+      }
+      else if (!$(this).hasClass("active")) {
+          new Audio("c.mp3").pause()
+          new Audio("c.mp3").currentTime = 0
+      }
+    }//
+
+
   });
 });
