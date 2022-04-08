bca98af52095df211ad65284980a1ef5f91202fb
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2022-04-09 07:46:16 +0900

    a

--- demo/2022/scroll-animation/main.js
+++ demo/2022/scroll-animation/main.js
@@ -1,7 +1,8 @@
+const SIZE = 140;
 var tmp = {};
 loadImageToTmp();
 function loadImageToTmp(){
-    for(var i=1;i<=100;i++){
+    for(var i=1;i<=SIZE;i++){
         const _i = i;
         const img = new Image();
         tmp[_i] = null;
@@ -14,20 +15,19 @@ function loadImageToTmp(){
 
 const image = document.getElementById("anim_img");
 
-var SIZE = 100;
-
-const offset = $("#anim_img_box").offset(); // 画像を入れるdiv要素
+const PX = 5; // 5px分の移動ごと画像を1枚進める
+const offset = $("#anim_img_box").offset(); // 画像を入れるdiv要素(position:stickyでトップに来たら固定される)
 $(window).scroll(function() {
-    const y = $(window).scrollTop();
-    const dy = y - offset.top;
-    if(offset.top<y&&y<offset.top+SIZE*5){
-        $("#anim_img_box").css("top",0)
-        const i = Math.floor(dy / 5);
-        if(i<=0||i>=SIZE) return;
-        if(tmp[i].src) image.src = tmp[i].src;
-    }else if(y>=offset.top+SIZE*5){
-        $("#anim_img_box").css("top","-"+(dy-SIZE*5))
-    }
+  const y = $(window).scrollTop();
+  const dy = y - offset.top;
+  if(offset.top<y&&y<offset.top+SIZE*PX){
+      $("#anim_img_box").css("top",0)
+      const i = Math.floor(dy / PX);
+      if(i<=0||i>=SIZE) return;
+      if(tmp[i].src) image.src = tmp[i].src;
+  }else if(y>=offset.top+SIZE*PX){
+      $("#anim_img_box").css("top","-"+(dy-SIZE*PX)); // スクロール分が終了したときに移動を始める
+  }
 });
 
-$("#anim_img_padding").height(SIZE*5); // スペースの変更
+$("#anim_img_padding").height(SIZE*PX); // スペースの変更
