7cc2870406b0b7194ba40d827bf33385c0d62220
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2022-04-09 11:26:40 +0900

    a

--- demo/2022/scroll-animation/main.js
+++ demo/2022/scroll-animation/main.js
@@ -16,7 +16,7 @@ function loadImageToTmp(){
 const image = document.getElementById("anim_img");
 
 const PX = 5; // 5px分の移動ごと画像を1枚進める
-const offset = $("#anim_img_box").offset(); // 画像を入れるdiv要素(position:stickyでトップに来たら固定される)
+const offset = $("#anim_img_box").offset();
 $(window).scroll(function() {
   const y = $(window).scrollTop();
   const dy = y - offset.top;
@@ -25,9 +25,9 @@ $(window).scroll(function() {
       const i = Math.floor(dy / PX);
       if(i<=0||i>=SIZE) return;
       if(tmp[i].src) image.src = tmp[i].src;
-  }else if(y>=offset.top+SIZE*PX){
-      $("#anim_img_box").css("top","-"+(dy-SIZE*PX)); // スクロール分が終了したときに移動を始める
-  }
+   }else if(y>=offset.top+SIZE*PX){
+       $("#anim_img_box").css("top","-"+(dy-SIZE*PX))
+   }
 });
 
 $("#anim_img_padding").height(SIZE*PX); // スペースの変更
