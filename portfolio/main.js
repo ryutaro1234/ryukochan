18b079da94f290951607478e4905084c032609bd
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2022-04-11 16:31:56 +0900

    update

--- portfolio/main.js
+++ portfolio/main.js
@@ -42,17 +42,17 @@ window.addEventListener("scroll", () => {
 
     const h1 = document.getElementsByTagName('h1');
     for (i = 0; i < h1.length; i++) {
-      h1[i].style.background = "linear-gradient(25deg, #333, #ff7f00)";
+      h1[i].style.background = "linear-gradient(25deg, #fff, #ffd6ad)";
       h1[i].style.webkitBackgroundClip = "text";
     }
     const h2 = document.getElementsByTagName('h2');
     for (i = 0; i < h2.length; i++) {
-      h2[i].style.background = "linear-gradient(25deg, #333, #ff7f00)";
+      h2[i].style.background = "linear-gradient(25deg, #fff, #ffd6ad)";
       h2[i].style.webkitBackgroundClip = "text";
     }
     const h3 = document.getElementsByTagName('h3');
     for (i = 0; i < h3.length; i++) {
-      h3[i].style.background = "linear-gradient(25deg, #333, #ff7f00)";
+      h3[i].style.background = "linear-gradient(25deg, #fff, #ffd6ad)";
       h3[i].style.webkitBackgroundClip = "text";
     }
 
@@ -74,17 +74,17 @@ window.addEventListener("scroll", () => {
 
     const h1 = document.getElementsByTagName('h1');
     for (i = 0; i < h1.length; i++) {
-      h1[i].style.background = "linear-gradient(25deg, #333, #ff3399)";
+      h1[i].style.background = "linear-gradient(25deg, #fff, #ffccff)";
       h1[i].style.webkitBackgroundClip = "text";
     }
     const h2 = document.getElementsByTagName('h2');
     for (i = 0; i < h2.length; i++) {
-      h2[i].style.background = "linear-gradient(25deg, #333, #ff3399)";
+      h2[i].style.background = "linear-gradient(25deg, #fff, #ffccff)";
       h2[i].style.webkitBackgroundClip = "text";
     }
     const h3 = document.getElementsByTagName('h3');
     for (i = 0; i < h3.length; i++) {
-      h3[i].style.background = "linear-gradient(25deg, #333, #ff3399)";
+      h3[i].style.background = "linear-gradient(25deg, #fff, #ffccff)";
       h3[i].style.webkitBackgroundClip = "text";
     }
 
