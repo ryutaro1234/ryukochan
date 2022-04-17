580c8dc4af060d2ec54d08974d9eb97e7ebcc202
Ryutaro Matsumoto <ryutarodayo1234@icloud.com>
2022-04-17 21:34:40 +0900

    update

--- main.js
+++ main.js
@@ -1,25 +1,33 @@
-window.addEventListener("scroll", () => {
-  const rect = document.getElementById('musicH1').getBoundingClientRect().top;
-  const scroll = window.pageYOffset || document.documentElement.scrollTop;
-  const offset = rect + scroll;
-  const windowHeight = window.innerHeight;
-  if (scroll > offset - windowHeight) {
-    document.getElementById('musicH1').classList.add("reveal-text");
+var Ease = {
+  easeInOut: function(t) {
+    return t < .5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
   }
-  else {
-      document.getElementById('musicH1').classList.remove("reveal-text");
-  }
-});
-
-window.addEventListener("scroll", () => {
-  const rect = document.getElementById('graphicH1').getBoundingClientRect().top;
-  const scroll = window.pageYOffset || document.documentElement.scrollTop;
-  const offset = rect + scroll;
-  const windowHeight = window.innerHeight;
-  if (scroll > offset - windowHeight) {
-    document.getElementById('graphicH1').classList.add("reveal-text");
-  }
-  else {
-    document.getElementById('graphicH1').classList.remove("reveal-text");
-  }
+}
+var duration = 500;
+window.addEventListener('DOMContentLoaded', function() {
+  var smoothScrollTriggers = [].slice.call(document.querySelectorAll('a[href^="#"]'));
+  smoothScrollTriggers.forEach(function(smoothScrollTrigger) {
+    smoothScrollTrigger.addEventListener('click', function(e) {
+      var href = smoothScrollTrigger.getAttribute('href');
+      var currentPostion = document.documentElement.scrollTop || document.body.scrollTop;
+      var targetElement = document.getElementById(href.replace('#', ''));
+      if (targetElement) {
+        e.preventDefault();
+        e.stopPropagation();
+        var targetPosition = window.pageYOffset + targetElement.getBoundingClientRect().top - 115;
+        var startTime = performance.now();
+        var loop = function(nowTime) {
+          var time = nowTime - startTime;
+          var normalizedTime = time / duration;
+          if (normalizedTime < 1) {
+            window.scrollTo(0, currentPostion + ((targetPosition - currentPostion) * Ease.easeInOut(normalizedTime)));
+            requestAnimationFrame(loop);
+          } else {
+            window.scrollTo(0, targetPosition);
+          }
+        }
+        requestAnimationFrame(loop);
+      }
+    });
+  });
 });
